a413690ec270e775e4c16547286594dab3aa3cfb
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-04 10:53:03 -0700

    for_Ab_ab

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Retun: 0 always successful
+ */
 int main(void)
 {
 char ltr;
-for (ltr = 'a'; ltr <= 'z'; ltr++ )
+for (ltr = 'a'; ltr <= 'z'; ltr++)
 {
 putchar(ltr);
 }
 for (ltr = 'A'; ltr <= 'Z'; ltr++)
 {
 putchar(ltr);
+}
+{
+putchar('\n');
 }
 return (0);
 }

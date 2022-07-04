e1a63327f6fe11959238fb1d846bd3eedd01bcdd
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-04 15:05:15 -0700

    forVariable_hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * Return: 0 always successful
+ */
+int main(void)
+{
+	int num;
+	char ltr;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	for (ltr = 'a'; ltr <= 'f'; ltr++)
+	{
+		putchar(ltr);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}

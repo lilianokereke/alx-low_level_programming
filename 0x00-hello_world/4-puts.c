992ac50f9a92354028f59cbe3466310e8f20ffc9
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-04 01:36:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/**main -> this is a function**/
+
+/**
+ * main :Entry point
+ *
+ * Return: Always 0 on succesful 
+ *
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }

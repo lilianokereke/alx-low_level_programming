dde1adf479d38077beac432ce3556576f1d89880
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-04 03:15:10 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main -Entry point
  * Description:starting at main, using printf to print,ending with return
- *  Return : Always 0 (success)
+ *  return -Always 0 (success)
 */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

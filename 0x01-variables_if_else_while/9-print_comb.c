3c0277591c6177f0476fb739632e8b42b38b4315
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-04 16:08:34 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,15 +4,18 @@
  * Return: 0 always successful
  */
 int main(void)
-
 {
 	int num;
 
 	for (num = 0; num <= 9; num++)
 {
-putchar((num % 10) + '0');				if (num == 9)
-	continue;												putchar(',');						putchar(' ');
+	putchar((num % 10) + '0');				if (num == 9)
+		continue;
+
+	putchar(',');
+
+	putchar(' ');
 }
-putchar('\n');
+	putchar('\n');
 return (0);
 }

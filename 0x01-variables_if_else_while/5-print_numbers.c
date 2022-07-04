c76330033243ca2651a0fda6524f645108de7fe5
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-04 11:03:33 -0700

    for_numb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 always successful
+*/
+int main(void)
+{
+char numb;
+for (numb = 0; numb <=10; numb++)
+{
+putchar(numb);
+}
+return (0);
+}

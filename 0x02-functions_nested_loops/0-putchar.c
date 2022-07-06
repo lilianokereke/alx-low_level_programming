60b1b3600ff9ba88367055a34aae96b620df55ab
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-06 03:49:22 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include "main.h"
 /**
- *
+ * main - Entry point
+ * Rerturn:0 is always successful
  *
  */
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
+	_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r')
+
+										_putchar('\n');
 	return (0);
 
 }

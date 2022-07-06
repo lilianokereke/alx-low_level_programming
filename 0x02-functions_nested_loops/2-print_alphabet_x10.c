8baa4f4e3ba88766d14de550e7a1edcaa30b9283
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-06 06:10:14 -0700

    nestedloood

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet_x10 - Entry point
+ */
+void print_alphabet_x10(void)
+	char i;
+	int num;
+	while (num++ < 9)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	{
+		_putchar('\n');
+	}
+}	

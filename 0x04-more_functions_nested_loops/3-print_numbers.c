6e3ec2fa2611c67da849aebe409c9866f63133e6
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-07 07:49:08 -0700

    countnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Printing number starting from 0 to 9
+ * Return : 0 always successful
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0 num >= 0; num++)
+		_putchar(num);
+		_putchar('\n');
+	return (0);
+}

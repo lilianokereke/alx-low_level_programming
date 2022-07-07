bf948fc249a85b84e8acc879f2c9aa759f962081
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-07 06:11:21 -0700

    isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,9 @@
 int _isupper(int c);
+void print_square(int size);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);

961de3113668e93673d5fe76795fcf3689c03810
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-12 11:21:53 -0700

    string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- *
- *
+ * print_rev - function to print reverse of s
+ * @s: argument for the function
  */
 void print_rev(char *s)
 {
-	while(*s++)
-		_putchar(--*s);
+	while (*s)
+
+		_putchar(*s++);
 	_putchar('\n');
 }

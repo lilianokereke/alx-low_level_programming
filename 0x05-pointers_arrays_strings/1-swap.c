cffe3ad176db5a96e42404d9f2833b52b3a03211
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-12 04:37:38 -0700

    swaping 2 pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * swap_int - to swap the 2 pointers
- * @a: the first parameter
- * @b: the second parameter
+ *
+ *
  */
 void swap_int(int *a, int *b)
 {
-	*a = *b;
+	int ptr;
+	*a = ptr;
 	*b = *a;
+	*a = *b;
 }
+

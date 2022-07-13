1e19826c71ca9190e3f893bd772939855fdcf101
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-13 12:08:14 -0700

    half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,17 +5,17 @@
  */
 void puts_half(char *str)
 {
-	int len = 0, half = 0, n = 0;
+	int len = 0, half = 0;
 
 	while (str[half++])
 	len++;
 
 	if ((len % 2) == 0)
 	{
-		n = len / 2;
+		len = len / 2;
 	}
 	else
-		n = (len - 1) / 2;
+		len = (len - 1) / 2;
 
 
 	for (half = 0; half < len ; half++)

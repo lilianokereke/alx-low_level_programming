2f0e27b96433ed9445608d5c123731f88c2c8983
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-13 12:49:02 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - to print an array
+ * @a: 1st parameter
+ * @n: 2nd parameter
+ */
+void print_array(int *a, int n)
+{
+	int arr[n];
+
+	printf("%d, \n", arr[n]);
+
+}

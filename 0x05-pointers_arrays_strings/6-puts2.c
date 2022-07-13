9ff4d385777062cd3028e5504dbdd9104df06f9f
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-13 10:32:26 -0700

    print one out of two

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,17 @@
 #include "main.h"
-/*
- * puts2 - out of two strings prints one  character
+/**
+ * puts2 - Prints one  char out of two of a string
  * @str: it contains the characters in the string
  */
 void puts2(char *str)
 {
-	int count = 0;
-	int len = 0;
+	int count = 0, len = 0;
 
 	while (str[count++])
 		len++;
 
 	for (count = 0; count < len; count += 2)
-			_putchar(str[count]);
+		_putchar(str[count]);
 
 	_putchar('\n');
 }

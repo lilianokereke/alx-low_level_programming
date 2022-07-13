782aa25cb53ddfd21d06f5f1c50d38dbf8876af2
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-13 07:31:15 -0700

    2nd character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,8 @@ void puts2(char *str)
 	while (str[count++])
 		len++;
 
-		for (count = 0; count < len; count += 2)
+	for (count = 0; count < len; count += 2)
 			_putchar(str[count]);
 
-					_putchar('\n');
+	_putchar('\n');
 }

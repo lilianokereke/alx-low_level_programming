bfd8eee596f1547b119f10aa4869d592b468a60f
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-13 07:26:58 -0700

    2nd character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
-/**
+/*
  * puts2 - out of two strings prints one  character
  * @str: it contains the characters in the string
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int count = 0;
 	int len = 0;
 
-	while (str[i++])
+	while (str[count++])
 		len++;
 
-		for (i = 0; i < len; i += 2)
-			_putchar(str[i]);
+		for (count = 0; count < len; count += 2)
+			_putchar(str[count]);
 
 					_putchar('\n');
 }

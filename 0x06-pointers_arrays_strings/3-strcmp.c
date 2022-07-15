04ffd852033775fcea17c56604d33c555db6f40e
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-15 03:05:39 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - compares 2 string
+ * @s1: first string
+ * @s2: 2nd string
+ * Return: returns a value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i] && s1[i] == '\0')
+		i++;
+			return (0);
+}

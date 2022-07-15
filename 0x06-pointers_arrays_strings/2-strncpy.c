d8bca190717341515c5c65ecec061a5eab943827
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-15 10:17:39 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, slen = 0;
-
-	while (dest[i++])
-		slen++;
-
+	int i = 0;
 		for (i = 0; i < n && src[i] != '\0'; i++)
 			dest[i] = src[i];
 
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] == s2[i] && s1[i] == '\0')
 		i++;
 	if (s1[i] == s2[i])
-		return (1);
-	else
 		return (0);
+	else
+		return (*s1 - *s2);
 }

410dc266fe1b5606da28ad02b573b6cf8593a2fa
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-20 12:42:35 -0700

    lenStr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * _strlen_recursion - print length of a string
+ * @s: string to check to get the length
+ * Return: return zero
  */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }

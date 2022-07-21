4b0bce32a94bbbb605143ddbbc6f555f0657a889
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-21 12:19:50 -0700

    prime num

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * sqr - che
+ * @g: same number as n
+ * @root: iterates from 1 to n
  *
- *
+ * Return: on success 1
  */
 int sqr(int g, int root)
 {
@@ -13,11 +15,11 @@ int sqr(int g, int root)
 	return (sqr(g + 1, root));
 }
 /**
- *
- *
- *
+ *_sqrt_recursion - reurns squareroot of n
+ * @n: number
+ * Return: on success 1
  */
- int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	return (sqr(1, n));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * check_prime - checks if a number is
+ * prime number
+ * @a:same number as n
+ * @b:number iterates from 1 to n
+ * Return: returns 1 if prime else 0
+ */
+int check_prime(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+			return (check_prime(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is * prime number
+ * @n: Number
+ * Return: returns 1 if prime else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+		return (check_prime(n, 2));
+}

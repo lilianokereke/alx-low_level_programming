98c1d840faa36abe5aead9f83122016d65f958eb
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-22 09:30:24 -0700

    mainArgv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,16 @@
 /**
  * main - entry point
  * @argc: number of argument
- * @agrv: array of strings
+ * @argv: array of strings
  * Return: it returns 0 on success
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	for (; i < argc; i++)
 	{
-	printf ("this is the name %s\n", argv[i]);
+	printf("this is the name %s\n", argv[i]);
 	}
 	return (0);
 }

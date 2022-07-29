2b7c75bed8a1a6a18ee670c672575424ded31f2d
lilianokereke <okerekeezinne2003@gmail.com>
2022-07-29 14:42:59 -0700

    commit array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * array_range - allocates memory to the array
- * @min: minimum/ first value
- * @max: maximum/last value
- * Return: a pointer
+  * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
  */
 int *array_range(int min, int max)
 

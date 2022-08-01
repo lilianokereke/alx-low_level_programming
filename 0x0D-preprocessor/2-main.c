897504d11f4dcfe37d8f5838552799f206b70cc9
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-01 04:26:22 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of file in which our code was compiled from followed by a new line
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**Header file that defines a function-like macro SUM(x,y) that computes the sum of the numbe * rs x and y.
+ */
+#define SUM(x, y) ((x) +  (y))
+
+#endif

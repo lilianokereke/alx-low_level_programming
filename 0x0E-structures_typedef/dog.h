1dbc7956ed34d37d2a485df5505bc586f9e415c6
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-01 05:27:13 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+#define DOG
+/**
+ * struct dog - data type
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

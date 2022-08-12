aec1d3d7e3087079838fdb06dbfd3ee9bc11bdd0
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-12 13:09:24 -0700

    end of list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,6 +5,7 @@
  * add_node_end - add a node at the end of a list
  * @head: the stringly list
  * @str: string in the list
+ * Return: the address of the new node or nul if fail
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -14,20 +15,17 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	size_t lent;
 
-	end == NULL;
-	
-
-
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-		new->str = strdup(str);
-		for (lent = 0; str[lent]; lent++);
-			new->len = lent;
-		new->next = end;
+	new->str = strdup(str);
+	for (lent = 0; str[lent]; lent++)
+		;
+		new->len = lent;
+		new->next = NULL;
 		end = *head;
 
 		if (end == NULL)

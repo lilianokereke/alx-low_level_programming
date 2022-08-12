e0768a4e8a8c2895aecbc4ff38e49e7f982ea99a
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-12 08:13:12 -0700

    add node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,24 +6,29 @@
  * @head: a pointer to the beginning of our list
  * @str: a string
  * Return: the address of our new node
- * Return: null if failed
+g* return: null if failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	char *strd;
+	size_t len;
 
 	strd = strdup(str);
 
-	*new = malloc(sizeof(list_t));
-	if (head != NULL)
+	new = malloc(sizeof(list_t));
+
+	if (new != NULL)
 	{
+		for (len = 0; str[len]; len++)
+			;
 		new->str = strd;
-		new->next = NULL;
-		new->next = (*head)->next;
-		(*head)->next = NULL;
+		new->next = *head; /*new pointer takes the place of the head*/
+		*head = new; /*takes the place of new pointer*/
 	return (new);
 	}
 	else
+	{
 		return (NULL);
+	}
 }

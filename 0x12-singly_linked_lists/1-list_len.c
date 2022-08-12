29da256d2551f8f6592eb2dce562f1f9e68cee05
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-12 08:43:44 -0700

    elements length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - get the number of elements in a linked list
+ * @h: the linked list
+ * Return: the number of element in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num++;
+
+	}
+	return (num);
+}

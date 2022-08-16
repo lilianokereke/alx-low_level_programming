e293d7fe51e22abe2fef1bba59df087deccc939a
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-16 02:05:18 -0700

    add at beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,8 +14,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	new->next = *head;
-	new->n = n;
-	*head = new;
+	else
+	{
+		new->next = *head;
+		new->n = n;
+		*head = new;
+	}
 	return (new);
 }

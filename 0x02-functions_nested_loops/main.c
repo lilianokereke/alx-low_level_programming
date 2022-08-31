49d3a9e492155ea3be1f4f1c435c2151ba86de08
lilianokereke <okerekeezinne2003@gmail.com>
2022-08-31 03:55:46 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+	{
+		ld*= -1;
+	}
+
+	_putchar(ld + '0');
+	_putchar('\n');
+	return (ld);
+}
+int main ()
+{
+	int x = 123;
+	int nm = print_last_digit(x);
+	return (0);
+}
